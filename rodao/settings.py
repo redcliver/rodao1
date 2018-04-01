52a668a5601900befcb65d2a29099cedec9427bd
redcliver <igor-peres@hotmail.com>
2018-04-01 16:23:23 -0400

    1.0

--- rodao/settings.py
+++ rodao/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 
 import os
 import posixpath
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -28,6 +29,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     'rodao.herokuapp.com',
+    'localhost',
     ]
 
 
@@ -86,8 +88,12 @@ WSGI_APPLICATION = 'rodao.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'rodao',
+        'USER': 'postgres',
+        'PASSWORD': '3355',
+        'HOST': '127.0.0.1',
+        'PORT': '', # 8000 is default
     }
 }
 
