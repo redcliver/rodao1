5131ffce7a921d950c223db45612d09e7f6314aa
redcliver <igor-peres@hotmail.com>
2018-04-16 18:40:22 -0400

    1.01

--- ordem/views.py
+++ ordem/views.py
@@ -363,10 +363,9 @@ def total_ordem(request):
        if request.method == 'POST' and request.POST.get('cliente_id') != None :
             cliente_id = request.POST.get('cliente_id')
             cliente_ord = cliente.objects.filter(id=cliente_id).get()
-            ordem_obj = ordens.objects.filter(cliente_ordem__id = cliente_id).all()
             tot_ordem = 0
-            for ordens in ordem_obj:
-                tot_ordem = tot_ordem + ordem_obj.total
+            for ordens in ordens.objects.filter(cliente_ordem__id = cliente_id).all():
+                tot_ordem = tot_ordem + ordens.total
             num_ordens = ordens.objects.filter(cliente_ordem = cliente_id).count()
             return render(request, 'total_ordem.html', {'title':'Total em Ordens','num_ordens':num_ordens, 'cli_ordem':cli_ordem, 'cliente_id':cliente_id, 'tot_ordem':tot_ordem})
            
