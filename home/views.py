ee84beeb32f409e425836102ef612c3b5b03f4f9
redcliver <igor-peres@hotmail.com>
2018-04-23 15:49:48 -0400

    1.01

--- home/views.py
+++ home/views.py
@@ -4,6 +4,7 @@ from django.utils import timezone
 from datetime import datetime
 from ordem.models import ordens
 from contas.models import conta
+from produto.models import produto
 
 # Create your views here.
 def home(request):
@@ -12,6 +13,7 @@ def home(request):
             ordem_aberta = ordens.objects.filter(estado=1).count()
         except:
             ordem_aberta = 0
+        prod = 0
         contas = 0
         hoje = timezone.now()
         for e in conta.objects.filter(estado=1).all():
@@ -19,8 +21,15 @@ def home(request):
                 contas = contas + 1
             else:
                 contas = contas
+
+        for p in produto.objects.all():
+            if p.quantidade <= p.qnt_minima:
+                prod = prod + 1
+            else:
+                prod = prod
         n_ordem = ordem_aberta
         n_contas =  contas
-        return render(request, 'home/index.html', {'title':'Home', 'n_ordem':n_ordem, 'n_contas':n_contas, 'hoje':hoje})
+        n_produto = prod
+        return render(request, 'home/index.html', {'title':'Home', 'n_ordem':n_ordem, 'n_contas':n_contas, 'hoje':hoje, 'n_produto':n_produto})
     else:
         return render(request, 'home/erro.html', {'title':'Erro'})
