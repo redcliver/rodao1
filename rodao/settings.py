fc056eda2726eb8bdc0e2c1655b58b86ec8420c0
redcliver <igor-peres@hotmail.com>
2018-04-23 09:53:54 -0400

    1.01

--- rodao/settings.py
+++ rodao/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 
 import os
 import posixpath
-#import dj_database_url
+import dj_database_url
 import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
