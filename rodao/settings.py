d9d26710d8a32645708ef86af79d889c189ceb5b
redcliver <igor-peres@hotmail.com>
2018-04-25 17:22:46 -0400

    1.01

--- rodao/settings.py
+++ rodao/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'b86e710c-c483-4721-ab00-5ca6de87f65d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     'rodao.herokuapp.com',
