d3d5e08161e484795bea6c02de81ea9eab077dc1
redcliver <igor-peres@hotmail.com>
2018-04-25 18:11:32 -0400

    1.01

--- ordem/views.py
+++ ordem/views.py
@@ -405,9 +405,9 @@ def total_mes(request):
             for j in ordens.objects.filter(cliente_ordem__id = cliente_id, estado='2', data_abertura__month=mes).all():
                 tot_ordem_1 = tot_ordem_1 + j.total
 
-            num_ordens = ordens.objects.filter(cliente_ordem = cliente_id, estado='1').count()
+            num_ordens = ordens.objects.filter(cliente_ordem = cliente_id, estado='1', data_abertura__month=mes).count()
 
-            num_ordens_1 = ordens.objects.filter(cliente_ordem = cliente_id, estado='2').count()
+            num_ordens_1 = ordens.objects.filter(cliente_ordem = cliente_id, estado='2', data_abertura__month=mes).count()
             clientes = cliente.objects.all().order_by('nome')
             return render(request, 'total_ordem.html', {'title':'Total em Ordens','num_ordens':num_ordens,'num_ordens_1':num_ordens_1,'tot_ordem_1':tot_ordem_1, 'cliente_ord':cliente_ord, 'cliente_id':cliente_id, 'tot_ordem':tot_ordem, 'clientes':clientes})
     else:
